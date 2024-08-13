47ed55ea9b2188ad620ac5a6160a533edc78769d
Pawel Maciulewski <pawel.maciulewski@gmail.com>
2024-08-13 10:02:09 +0200

    error boundaries

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,6 +70,8 @@ redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id:string) {
+  throw new Error('Failed to Delete Invoice');
+
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
